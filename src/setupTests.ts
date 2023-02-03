3f43b0d5882b582d9190a61948afac16821a1725
dmitriyyan <dmitriy.ya@protonmail.com>
2023-02-03 20:13:50 +0300

    chore: add and configure RTL and vitest

---
+++ src/setupTests.ts
@@ -0,0 +1,9 @@
+// add a test setup file in setupTests.ts
+import { afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+// runs a cleanup after each test case (e.g. clearing jsdom)
+afterEach(() => {
+  cleanup();
+});
